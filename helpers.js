6b77d27add35e61e1f3241d83b54834dd2660a63
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-18 18:54:25 +0530

    Only Julie Mwol ✌🥳

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *Julie Mwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *Julie Mwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
 }
 
 
