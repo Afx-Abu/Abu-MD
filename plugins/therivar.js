a3a8747473ac3a45402c2df7483a6ffaafadd37d
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-18 19:06:50 +0530

    Fix 🙌

--- plugins/therivar.js
+++ plugins/therivar.js
@@ -17,7 +17,7 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
     var THERI_off = ''
    
     if (config.LANG == 'EN') {
-        l_dsc = 'To remove members when they use bad language'        
+        l_dsc = 'To remove members when they use bad words'        
         THERI_on = 'ʙᴀᴅ ᴡᴏʀᴅ ʀᴇᴍᴏᴠɪɴɢ ɪꜱ ᴛᴜʀɴᴇᴅ ᴏɴ'
         THERI_off = 'ʙᴀᴅ ᴡᴏʀᴅ ʀᴇᴍᴏᴠɪɴɢ ɪꜱ ᴛᴜʀɴᴇᴅ ᴏꜰꜰ'
     }
