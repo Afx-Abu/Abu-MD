22e5eeb7dbaf23e821c09532dc8b90238ddf26bc
Hisham-Muhammed <87166264+Hisham-Muhammed@users.noreply.github.com>
2021-08-18 16:41:59 +0530

    Hyper sir🙂😁

--- bot.js
+++ bot.js
@@ -240,7 +240,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ 𝕁𝕦𝕝𝕚𝕖𝕄𝕨𝕠𝕝 ~______~*' +
+                                await conn.sendMessage(conn.user.jid, '*~_________~ ғɪᴅͥʜͭᴀᷤ ᴍͫᴡⷷᴏʟ ~______~*' +
                                     '\n\n*🧞‍♂️ ' + error + '*\n'
                                     , MessageType.text);
                             }
