9c2e3f52e1460e45cc38e90edc95d161f1277e41
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-08-26 14:35:04 +0530

    Create fidhamol.js

---
+++ plugins/fidhamol.js
@@ -0,0 +1,27 @@
+const Asena = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('unvoice'); // Language support
+
+Asena.addCommand({pattern: 'voice ?(.*)', fromMe: true, desc: Lang.UV_DESC}, (async (message, match) => {    
+    if (message.reply_message === false);
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+let id = match[1];
+    ffmpeg(location)
+        .format('mp3')
+        .save('output.mp3')
+        .on('end', async () => {
+            await message.client.sendMessage(id, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio,duration: 9999999,contextInfo: { forwardingScore: 5, isForwarded: true }, ptt: true});
+});}));
