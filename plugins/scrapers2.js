0db50cde3c05956028ecdec2ad680f04d45937d7
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-08-31 23:53:21 +0530

    Update scrapers2.js

--- plugins/scrapers2.js
+++ plugins/scrapers2.js
@@ -244,7 +244,7 @@ else if (config.WORKTYPE == 'public') {
 	msg += 'Production : ' + json.Production + '\n\n';
 	msg += 'imdbRating : ' + json.imdbRating + '\n\n';
 	msg += 'imdbVotes  : ' + json.imdbVotes + '```';
-        msg += 'link       : ' + json.link + '\n*You could get requested movies from here*\n\nhttps://t.me/malayalam_rocker_s*';
+        msg += 'link       : ' + json.link + '\n*You could get requested movies from here*\n\nhttps://t.me/malayalam_rocker_s';
 	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
 }));
 
@@ -262,13 +262,13 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    Asena.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+    Asena.addCommand({pattern: 'vava ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*Mwolus* 💗' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*Vava* 💗' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
