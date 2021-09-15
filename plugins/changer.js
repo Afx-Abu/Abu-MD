2464ed4a08566722a5c01e13d6a806fea0eb22c2
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-09-15 13:52:28 +0530

    Other chance to get  erorr

--- plugins/changer.js
+++ plugins/changer.js
@@ -17,7 +17,7 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
     
     plk_desc = 'change reply message BGM mode'
     BGM_ONE = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝗮𝐧𝐠𝐞𝐝 𝐭𝐨 𝟭𝘀𝘁 𝐦𝐨𝐝𝐞 𝐏𝐥𝐞𝐚𝐬𝐞 𝐰𝐚𝐢𝐭 5 𝐌𝐢𝐧 𝐭𝐨 𝐫𝐞𝐬𝐭𝐚𝐫𝐭'
-    BGM_TWO = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝗮𝐧𝐠𝐞𝐝 𝐭𝐨 𝟐𝐧𝐝 𝐦𝐨𝐝𝐞 𝐏𝐥𝐞𝐚𝐬𝐞 𝐰𝐚𝐢𝐭 5 𝐌𝐢𝐧 𝐭𝐨 𝐫𝐞𝐬𝐭𝐚𝐫𝐭 '
+    BGM_TWO = '𝐁𝐆𝐌 𝐭𝐲𝐩𝐞 𝐜𝐡𝗮𝐧𝐠𝐞𝐝 𝐭𝐨 𝟐𝐧𝐝 𝐦𝐨𝐝𝐞 𝐏𝐥𝐞𝐚𝐬𝐞 𝐰𝐚𝐢𝐭 5 𝐌𝐢𝐧 𝐭𝐨 𝐫𝐞𝐬𝐭𝐚𝐫𝐭 ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ᴛɪᴄᴋ ᴀᴅᴅᴇᴅ ✅️'
     }
 
     if (config.LANG == 'ML') {
