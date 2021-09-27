6b2e27c3e4466023f6a4316ea5c5703572c76ebe
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-09-27 18:45:48 +0530

    Update now

--- plugins/reels.js
+++ plugins/reels.js
@@ -17,7 +17,7 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from i
     var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
     
    const { status, type, data } = await instagram(match[1], 'julie')
-    if (!status) return await message.sendMessage('*✅️Example : https://www.instagram.com/p/CCdcH3FBd1a/?utm_medium=copy_link\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Farhan or Amal*')
+    if (!status) return await message.sendMessage('✅️Example : https://www.instagram.com/p/CCdcH3FBd1a/?utm_medium=copy_link\n\n*Change /reel/ to /p/ Then give Command .insta*\n\n*Any doubt ask to Farhan or Amal*')
 
     reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
     
