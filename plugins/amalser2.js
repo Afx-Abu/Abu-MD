711bbd9f8bbb143bdead42b0ca02cb089b7c2814
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-09-30 20:41:38 +0530

    Subcribe my channel

--- plugins/amalser2.js
+++ plugins/amalser2.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Amalser - Amal
+Amalser - Amal,farhan
 */
 
 const Asena = require('../events');
