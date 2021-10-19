8a36f89f659641163de330dce9a37c45b186ebc1
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-19 12:43:43 +0530

    Delete forward1.js

--- plugins/forward1.js
+++
@@ -1,8 +0,0 @@
-Asena.addCommand({ pattern: "f ?(.*)", fromMe: true, desc: 'Forwards replied audio' },
-  async (message, match) => {
-    if (match == "") return await message.sendMessage('Give any JID');
-    if (!message.reply_message) return await message.sendMessage('```Forwarding audio...```');
-    match.match(parseJid).map((jid) => {forwardOrBroadCast(match[1], message);
-    });
-  }
-);
