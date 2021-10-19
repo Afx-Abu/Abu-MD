04c52221496baa4d7ae2b622965fc8911112c460
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-19 15:52:06 +0530

    ✅️

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/1vi0Bpm.mp4";
+  r_text[0] = "https://i.imgur.com/6G31RxX.jpeg";
     
     
     var i = Math.floor(1*Math.random())
