aa6943e268b8c9af06ad760b7ffaa68cec32ac87
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-20 16:24:39 +0530

    Update python.js

--- plugins/python.js
+++ plugins/python.js
@@ -1,23 +1,17 @@
-/* Copyright (C) 2021 Amalser.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Amalser - Amal
-Wa.me/+919895828468
-*/
-
-const Neotro = require('../events');
+const WhatsAlexa = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Config = require('../config')
 const axios = require('axios')
 const request = require('request');
-const { getBuffer } = require("../White/cofig");
 const os = require('os');
 var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 
- WhatsAlexa.addCommand({pattern: 'btm', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+let whb = Config.WORKTYPE == 'public' ? false : true
+
+WhatsAlexa.addCommand({pattern: 'btm', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
