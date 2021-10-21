a2d76b5a6d0d4c198608db16520bb5f0fb645cda
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-10-21 12:09:05 +0530

    Update python.py

--- languages/python.py
+++ languages/python.py
@@ -1,1 +1,6 @@
-## updating 
+
+# This program is updating it will not work properly! Don't re mod and show massive of your skills🥴
+## coding utf - ©️ sunaif-adkar
+
+print('Hey It's me Amal Ser')
+
