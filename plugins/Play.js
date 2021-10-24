4215a693cf770e979fc17c0e49ee8261e99a2f96
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-24 22:10:46 +0530

    Create Play.js

---
+++ plugins/Play.js
@@ -0,0 +1,32 @@
+const Asena = require('../events');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+const LOAD_ING = "*RESULT FOUND UPLOADING...* \n\n *🛑Wait.....*"
+const axios = require('axios')
+const Axios = require('axios')
+
+
+Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+	
+	await message.client.sendMessage(message.jid, '*SEARCHING YOU DATA*' , MessageType.text, { quoted: message.data });
+	
+	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
+	
+        const { status, result } = data
+	
+	var img = await Axios.get(`${result.thumb}`, {responseType: 'arraybuffer'})
+	
+        if(!status) return await message.sendMessage('*NO RESULT FOUND*')
+	
+        await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
+        let msg = '```'
+        msg +=  `TITLE :${result.title}\n\n`
+        msg +=  `THUMBNAIL :${result.thumb}\n\n`
+        msg +=  `CHANNEL :${result.channel}\n\n`
+        msg +=  `DATE OF PUBLISHED :${result.published}\n\n`
+        msg +=  `TOTAL VIEWS :${result.views}\n\n`
+        msg +=  `DOWNLOADING LINK :${result.url}\n\n`
+        msg += '```'
+         return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.image, {mimetype: Mimetype.jpg , caption: msg })
+        });
