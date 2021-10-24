69856c575a2f0eda9f1b61ddfbab1ba9860e53f7
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-24 22:27:58 +0530

    song thumbnail adedd

--- plugins/Play.js
+++ plugins/Play.js
@@ -2,14 +2,14 @@ const Asena = require('../events');
 const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const got = require('got');
 const Config = require('../config');
-const LOAD_ING = "*RESULT FOUND UPLOADING...* \n\n *🛑Wait.....*"
+const LOAD_ING = "*wait...*"
 const axios = require('axios')
 const Axios = require('axios')
 
 
-Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
 	
-	await message.client.sendMessage(message.jid, '*SEARCHING YOU DATA*' , MessageType.text, { quoted: message.data });
+	await message.client.sendMessage(message.jid, '*Searching.....*' , MessageType.text, { quoted: message.data });
 	
 	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
 	
