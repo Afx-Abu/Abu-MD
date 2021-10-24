66d62fa94bb467b98c05c99fe64fa85a178f3d4c
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-24 21:14:46 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -111,6 +111,19 @@ else if (Config.WORKTYPE == 'public') {
         await message.client.sendMessage(message.jid,tx, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }
 }));
+var stag_dsc = ''
+if (Config.LANG !== 'ML') stag_dsc = 'Sends the replied message to all members in the group.'
+if (Config.LANG == 'ML') stag_dsc = 'ഗ്രൂപ്പിലെ എല്ലാ അംഗങ്ങൾക്കും മറുപടി സന്ദേശം അയയ്ക്കുന്നു.'
+
+Asena.addCommand({pattern: 'group bc$', fromMe: true, desc: stag_dsc }, (async (message, match) => {
+    if (!message.reply_message) return await message.client.sendMessage(message.jid,SLang.NEED_REPLY, MessageType.text)
+    grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(async (uye) => {
+        await message.client.sendMessage(uye.jid, message.reply_message.text, MessageType.text)
+    })
+}));
 
 }
 
