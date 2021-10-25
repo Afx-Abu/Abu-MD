3b5463ac6515aababc7769fba720c92bedacc73a
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-25 22:08:57 +0530

    Dont update

---
+++ plugins/testingonly.js
@@ -0,0 +1,31 @@
+const WhatsAlexa = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+var ggg = Buffer.from(clh.cd, 'base64')
+var ddd = ggg.toString('utf-8')
+
+let whb = Config.WORKTYPE == 'public' ? false : true
+
+WhatsAlexa.addCommand({pattern: 'afamk', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+// send a buttons message!
+    const buttons = [
+        {buttonId: 'id1', buttonText: {displayText: 'i am fine 🥰'},{displayText: 'i am '}, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: 'Do you like Bot'},{displayText: 'i am 🥰'}, type: 1},
+        {buttonId: 'id3', buttonText: {displayText: 'Hii'},{displayText: 'i fine 🥰'}, type: 1}
+      ]
+      
+      const buttonMessage = {
+          contentText: "Hi How Are You ?",
+          footerText: 'owner Amalser  ©',
+          buttons: buttons,
+          headerType: 1
+      }
+      
+      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
+
+}));
