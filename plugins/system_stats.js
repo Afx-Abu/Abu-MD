3d6631b34797bf93d6aa933a3eaf957975924bb4
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-28 00:09:49 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -43,13 +43,12 @@ else if (Config.WORKTYPE == 'public') {
         var plk_here = new Date().toLocaleDateString(get_localized_date)
 	var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '```'
 
-{buttonId: 'id1', buttonText: {displayText: 'i am fine 🥰'}, type: 1},
+        {buttonId: 'id1', buttonText: {displayText: 'i am fine 🥰'}, type: 1},
         {buttonId: 'id2', buttonText: {displayText: 'Do you like Bot'}, type: 1},
         {buttonId: 'id3', buttonText: {displayText: 'Hii'}, type: 1}
       ]
       
-      const buttonMessage = {
-          contentText: "Hi How Are You ?",
+      const buttonMessage = {      
           footerText: 'owner Amalser  ©',
           buttons: buttons,
           headerType: 1
