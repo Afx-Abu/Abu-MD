78af2dc1c7a6c5b2e4bdba55c57c7347b6320f48
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-10-28 20:42:54 +0530

    Update Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -8,7 +8,7 @@ const Axios = require('axios')
 
 Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true },async (message, match) => {
 	
-	await message.client.sendMessage(message.jid, '*Wait*' MessageType.text, { quoted: message.data });
+	await message.client.sendMessage(message.jid, '*Wait*',MessageType.text, { quoted: message.data });
 	
 	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
 	
