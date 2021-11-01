d87112470008e096ce794eeef688ef10df0da143
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-11-01 17:36:38 +0530

    Update 8.00pm

---
+++ amalbot.js
@@ -0,0 +1,17 @@
+/* codded by afnanplk
+*/
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
+ PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
+};
