7c91c9bced05e5aa33565851ae5ed93a402df377
Cyber Chekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-11-02 11:31:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ async function whatsAsena () {
     
     
     const conn = new WAConnection();
-    conn.version = [2, 2126, 14];
+    conn.version = [3,3234,9];
     const Session = new StringSession();
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
