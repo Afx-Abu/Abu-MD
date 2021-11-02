5fb38e9b6f0825fdd5743437c0e72a59c9610a3e
Maurício Aguiar <78458084+mauriaguiar@users.noreply.github.com>
2021-11-02 03:47:59 -0300

    UPDATED QR CODE !!
    Fixed!!

--- julie.js
+++ julie.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [2, 2119, 6]
+    conn.version = [2, 2140, 12]
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
     
