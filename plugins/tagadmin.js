63aa097ec92f57997aa35821f1bcff3a3e24cf7b
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-04 22:58:53 +0530

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -17,7 +17,7 @@ if (Config.WORKTYPE == 'private') {
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                mesaj += '✅️@' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                mesaj += '✅️@' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
