56975ffcaeff1143ed4876fc23bdcbda303e1f7a
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-07 01:31:04 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -3,7 +3,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
-Pinky.addCommand({pattern: 'number', fromMe: false, desc: Lang.NUMBER}, (async (message, match) => {
+Pinky.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 const Pinky = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:' + Pinky.OA_NAME + '\n' //created afnanplk, please copy this with credit..
