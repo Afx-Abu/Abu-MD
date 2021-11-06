89128a7208f7df79c48e2c6d14a576b8bdd04766
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-07 01:22:58 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -3,7 +3,6 @@ const {MessageType} = require('@adiwajshing/baileys');
 const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
-const Pinky = require('../Pinky')
 Pinky.addCommand({pattern: 'number', fromMe: false, desc: Lang.NUMBER}, (async (message, match) => {
 const Pinky = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
