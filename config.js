6260aaa0d6ab161a7f74a861a39263f925420ab0
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-06 16:43:02 +0530

    EMOJI_WARNING,EMOJI_DESCRIPTION, EMOJI_COMMAND

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ module.exports = {
     EMOJI_DESCRIPTION: process.env.EMOJI_DESCRIPTION === undefined ? '🦸‍♂️' : process.env.EMOJI_DESCRIPTION,
     EMOJI_EXAMPLE: process.env.EMOJI_EXAMPLE === undefined ? '💬' : process.env.EMOJI_EXAMPLE,
     EMOJI_WARNING: process.env.EMOJI_WARNING === undefined ? '⚠️' : process.env.EMOJI_WARNING,
-    PANEL_COMMAND: process.env.PANEL_COMMAND === undefined ? 'help' : process.env.PANEL_COMMAND,
+    PANEL_COMMAND: process.env.PANEL_COMMAND === undefined ? 'menu' : process.env.PANEL_COMMAND,
     WEL_GIF: process.env.WEL_GIF === undefined ? 'https://i.imgur.com/nErXUGj.mp4' : process.env.WEL_GIF,
     GIF_BYE: process.env.GIF_BYE === undefined ? 'https://i.imgur.com/Z1jCYGN.mp4' : process.env.GIF_BYE,
     HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
