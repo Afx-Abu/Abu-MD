75c58e9a4b0e281d227aa3842481fa2fc7411558
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-07 00:53:32 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -5,14 +5,13 @@ const os = require("os");
 const fs = require('fs');
 const Pinky = require('Pinky');
 const Config = require('../config')
-Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
-
+Pinky.addCommand({pattern: 'number', fromMe: false, desc: Lang.NUMBER}, (async (message, match) => {
 //coded by saidali
-const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+const Pinky = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:' + Pinky.OA_NAME + '\n' //created afnanplk, please copy this with credit..
             + 'ORG:pinky julie fam;\n'
             + 'TEL;type=CELL;type=VOICE;waid=' + Pinky.PHONE + ':' + Pinky.PHONE + ' \n'
             + 'END:VCARD'
-await message.client.sendMessage(message.jid, {displayname: "PINKY", vcard: p_lk}, MessageType.contact);
+await message.client.sendMessage(message.jid, {displayname: "PINKY", vcard: Pinky}, MessageType.contact);
 }))
