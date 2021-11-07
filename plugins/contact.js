d8f3676ee99032ca341b0820728cad39a22e11af
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-07 07:05:34 +0530

    Dont update

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,8 +9,8 @@ Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (a
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Amal [OWNER]\n' // full name
-            + 'ORG:Amal;\n' // 
+            + 'FN:Amalser [OWNER]\n' // full name
+            + 'ORG:Amalser;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
