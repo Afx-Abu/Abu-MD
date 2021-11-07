b6df8308b3f14e57c8e52324abdd8bb893424d70
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-07 06:30:43 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -1,16 +1,1 @@
-const Amal = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const exec = require('child_process').exec;
-const os = require("os");
-const fs = require('fs');
-const config = require('../config');
-const Pinky = require('../pinky')
-Amal.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
-const Pinky = 'BEGIN:VCARD\n' // metadata of the contact card
-            + 'VERSION:3.0\n' 
-            + 'FN:' + Pinky.OA_NAME + '\n' //created afnanplk, please copy this with credit..
-            + 'ORG:pinky julie fam;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=' + Pinky.PHONE + ':' + Pinky.PHONE + ' \n'
-            + 'END:VCARD'
-await message.client.sendMessage(message.jid, {displayname: "PINKY", vcard: Pinky}, MessageType.contact);
-}))
+
