72e33636c8bb0f59ae1885b1a64b7ca6be1d2f96
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-07 06:59:16 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -1,1 +1,17 @@
+const Ktb = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
+//coded by saidali
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Amalser [OWNER]\n' // full name
+            + 'ORG:Amalser;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+}))
