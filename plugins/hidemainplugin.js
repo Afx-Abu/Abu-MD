10a8e486d8e4e7079389fa025b787c7163c63f93
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-19 23:49:26 +0530

    Update hidemainplugin.js

--- plugins/hidemainplugin.js
+++ plugins/hidemainplugin.js
@@ -12,13 +12,13 @@ var ddd = ggg.toString('utf-8')
 
 let whb = Config.WORKTYPE == 'public' ? false : true
 
-WhatsAlexa.addCommand({pattern: 'mkmods', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'live', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!
 var r_text = new Array ();
     
   
 r_text[0] = "https://i.imgur.com/w89FHm7.jpeg";
- r_text[1] = Config.LG_LOGO
+r_text[1] = Config.LG_LOGO
    
 var i = Math.floor(2*Math.random())
     const buttons = [
@@ -27,7 +27,7 @@ var i = Math.floor(2*Math.random())
       ]
       
       const buttonMessage = {
-          contentText: ' BOT NAME:``` *'+Config.BOT+'*\n\n```*'+Config.ALIVEMSG+'*\n',
+          contentText: ' ``` '+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n',
           footerText: 'Amalser ©',
           buttons: buttons,
           headerType: 1
