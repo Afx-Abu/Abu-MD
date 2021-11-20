e080c26e52222f1a55e2d8d22d2d33c899a8fe8d
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-20 19:49:03 +0530

    Update bot now all erorr fix hope you enjoy

--- plugins/database.js
+++ plugins/database.js
@@ -20,7 +20,7 @@ var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '
 	const buttons = [
 
         {buttonId: 'id1', buttonText: {displayText: 'OWN\n\n*Amalser v2 two types of alive message*\n\n*Hi i am live {pp}*\n\n*Hi i am alive {qt}*\n\n*welcome message addedd simple way*\n\n*.welcome {pp} {gphead} {gpmaker} {gpdesc}{owner}*\n\n*Another way*\n*.welcome {gif} {gphead} {gpmaker} {gpdesc} {time} {owner}*\n\n*Broadcast adedd replay with any message .bc*\n\n*Alive message time set*\n\n*Button message added .bot and .help*\n\n*All type of downloading command example .yt, .video, .song, .get*\n\n '}, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: 'GIT\n\n```youtube Video: https://youtu.be/5MKycJxmA4c```\n\n```Github link: https://github.com/Amal-ser/Amalser```\n\n```YT_PASSWORD:  password in youtube channel video watch it full and subcribe for more updations```\n'  }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: 'GIT\n\n```youtube Video: https://youtu.be/5MKycJxmA4c```\n\n```Github link: https://github.com/Amal-ser/Amalser```\n\n```YT_PASSWORD:  password in youtube channel video watch it full and subcribe for more updations```\n\n```For making photo to link - https://t.me/HK_telegraph_BOT```\n'  }, type: 1},
 
       ]
       
