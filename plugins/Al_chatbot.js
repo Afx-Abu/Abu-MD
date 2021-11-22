c3c8d3b214379f25391a0ae2ba1e1a0ae384785a
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-22 22:34:46 +0530

    Update

--- plugins/Al_chatbot.js
+++ plugins/Al_chatbot.js
@@ -234,7 +234,7 @@ if (conf.LANG == 'ML') {
     succ_off = 'Amalser സെമി-ഫങ്ഷണൽ ആയി സജ്ജമാക്കുക! കുറച്ച് കാത്തിരിക്കൂ! ☑️'
 }
 
-New.addCommand({ pattern: 'amalser ?(.*)', desc: _dsc, fromMe: true,dontAddCommandList: true, usage: '.chatbot on / off' }, (async (message, match) => {
+New.addCommand({ pattern: 'amalser ?(.*)', desc: _dsc, fromMe: true,dontAddCommandList: true, usage: '.amalser on / off' }, (async (message, match) => {
     var eva_status = `${conf.CHATBOT}`
     if (match[1] == 'on') {
         if (eva_status == 'true') {
