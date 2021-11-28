ca861d104f2fbaa3e2dbe7086bca7fcb4720ae2d
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-11-28 13:14:04 +0530

    Update all erorrr fix by Amalser ✅️

--- plugins/xlove.js
+++ plugins/xlove.js
@@ -9,12 +9,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://i.imgur.com/w89FHm7.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Amalser*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name: Amalser*
+
 *Creator number : wa.me/919895828468?text=Hi%20Amal%20Ser.%20*
 
  *To check update .update   To update Bot .update now*
 
- *Bot making video : https://youtu.be/5MKycJxmA4c*
+ *Bot making video : https://youtu.be/Tb1B-rS52uo*
      
  *Instagram id: https://www.instagram.com/p/CCdcH3FBd1a/?utm_medium=copy_link*
 
