d327299104f442ced902cd1c280b9578fc31a2f0
SKL!! <87891508+souravkl11@users.noreply.github.com>
2021-12-09 19:34:51 +0530

    Hm

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Amalser.
+/* Copyright (C) Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Remoded by - Amal
