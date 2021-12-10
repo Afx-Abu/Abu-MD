879d36fb7fec9b25fcddcc58436fb70e925939a9
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-12-10 22:55:48 +0530

    Update hidemainplugin.js

--- plugins/hidemainplugin.js
+++ plugins/hidemainplugin.js
@@ -23,8 +23,8 @@ let whb = Config.WORKTYPE == 'public' ? false : true
 Amalser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!credit Amal
     const buttons = [
-        {buttonId: 'id1', buttonText: {displayText: sk1 }, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: sk2 }, type: 1}
+        {buttonId: 'id1', buttonText: {displayText: +  sk1  + }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: +  sk2  + }, type: 1}
       ]
       
       const buttonMessage = {
