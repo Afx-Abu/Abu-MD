f03740eec9d8a48bcdbc7db85388769172d1a305
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-01-15 16:04:40 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -94,7 +94,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Mᴀᴅᴇ Bʏ Aʙᴜ Sᴇʀ'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Mᴀᴅᴇ Bʏ Aᴍʀᴜ Sᴇʀ'})
 
     }));
 }
@@ -188,7 +188,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Mᴀᴅᴇ Bʏ Aʙᴜ Sᴇʀ Bᴏᴛ'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Mᴀᴅᴇ Bʏ Aᴍʀᴜ Sᴇʀ Bᴏᴛ'})
 
     }));
 }
