87301f087fd71c7d9245afd894a0307af4794066
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-01-15 16:29:31 +0530

    Update jasil.js

--- plugins/jasil.js
+++ plugins/jasil.js
@@ -7,20 +7,21 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://www.linkpicture.com/q/IMG-20220108-WA0143.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/CtaexU3.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bᴏᴛ Nᴀᴍᴇ 🇦 🇧 🇺  🇸 🇪 🇷  🇧 🇴 🇹 *
 
-*Cʀᴇᴀᴛᴇʀ number : wa.me/917025994178?text=Hi%20ᴀʙᴜ%20bro.%20*
-
+ *Cʀᴇᴀᴛᴇʀ number : wa.me/917025631103?text=Hi%20Amruser%20bro.%20*
 
  *Kᴀʟᴀɴ sᴇʀ Cʜᴀɴᴀʟ : https://youtu.be/OYhA_ZNQ4GQ*
      
- *Iɴsᴛᴀɢʀᴀᴍ ɪᴅ: https://instagram.com/_.jasil_rx?utm_medium=copy_link*
+ *Cʀᴇᴀᴛᴇʀ ɴᴀᴍᴇs: Aᴍʀᴜ sᴇʀ  & Aʙᴜ sᴇʀ *
 
- *Kᴀʟɴ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/Bq0eHs3UpGJ2BKIHOmy7mk*
+ *Oᴡɴᴇʀ : Aᴍʀᴜ sᴇʀ Bᴏᴛ ᴘʜ:» wa.me/917025631103*
 
- *Gɪᴛ Lɪɴᴋ : https://github.com/Arx-Abu/Abu_ser*
+ *Gɪᴛ Lɪɴᴋ Fᴏʀ Aᴍʀᴜ Sᴇʀ : https://github.com/AMRUSIR/AMRU-SER*
+
+ *Gɪᴛ Lɪɴᴋ Fᴏʀ Aʙᴜ Sᴇʀ : https://github.com/Arx-Abu/Abu_ser*
 
  *Aʙᴜ-ʙᴏᴛꫂ⁩..♡︎*
 `}) 
