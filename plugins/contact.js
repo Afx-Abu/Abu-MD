b110f4640d71f504a4ef32e89148059a039104be
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-01-16 14:31:09 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,9 +9,9 @@ Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (a
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:AMRU SER [OWNER]\n'// full name
-            + 'ORG:AMRU SER;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=917025631103:\n' // WhatsApp ID +917025631103
+            + 'FN:AMRU-SER [OWNER]\n' // full name
+            + 'ORG:AMRU-SER;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=917025631103:+91 7025994178\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "AMRU SER [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "AMRU-SER [OWNER]", vcard: vcard}, MessageType.contact)
 }))
