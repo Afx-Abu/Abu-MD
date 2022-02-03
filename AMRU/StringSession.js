16cd514ffedae28dc6874b3f77125449ca562c22
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-02-03 21:03:55 +0530

    Update StringSession.js

--- AMRU/StringSession.js
+++ AMRU/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('AMALSER_CODE' in process.env && string === undefined) {
+        if ('AMRUSER_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Amalser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Amruser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
