68d5f61d8dcd04d6dbe38d6767cae2df0c6f4cb7
AMRUSIR <92585914+AMRUSIR@users.noreply.github.com>
2022-05-12 06:50:20 +0530

    𝙵𝙸𝚇

--- plugins/abubotv10.js
+++ plugins/abubotv10.js
@@ -20,17 +20,17 @@ let whb = Config.WORKTYPE == 'public' ? false : true
 LizaMwol.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!credit Amal
     const buttons = [
-        {buttonId: 'id1', buttonText: {displayText: Config.ALIVE_BUTTON }, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: Config.ALIVEBUTTON }, type: 1}
+        {buttonId: 'id1', buttonText: {displayText: '𝙼𝙴𝙽𝚄' }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: '𝙾𝚆𝙽𝙴𝚁' }, type: 1}
       ]
       
       const buttonMessage = {
-          contentText: '```'+Config.BOTV2+'\n\n```'+Config.ALIVEMSG+'\n',
+          contentText: '```'+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n',
           footerText: 'ᴀᴍʀᴜ sᴇʀ™ ',
           buttons: buttons,
           headerType: 1
       }
       
-await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 123, status: 200, thumbnail: fs.readFileSync('https://www.linkpicture.com/q/20220115_085729.jpg'), surface: 200, message: Config.BOTV2, orderTitle: Config.BOTV2, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOTV2 + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('https://www.linkpicture.com/q/20220115_085729.jpg')}}}});
+await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 123, status: 200, thumbnail: fs.readFileSync('Amru.jpg'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('Amru.jpg')}}}});
 
 }));
