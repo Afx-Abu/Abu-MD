8e0a42358dc12d6d3e175bd762fd2c4d2cd46353
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-04-13 11:21:16 +0530

    fix

--- plugins/song.js
+++ plugins/song.js
@@ -1,7 +1,8 @@
 const {
   Module,
   isPublic,
-  getSong
+  getSong,
+  getVideo
 } = require("../lib/");
 
 Module
@@ -9,12 +10,26 @@ Module
 		{
             pattern: "song?(.*)",
 	    fromMe: isPublic,
-	    desc: "show your menu items",
+	    desc: "downloading your YouTube song",
             type: "downloader",
             },
 		async (message, match) => {
                 if(!match) return await message.reply("_enter song url/name_")			
 		return await getSong(message, match);
+
+})
+
+Module
+	(
+		{
+            pattern: "video?(.*)",
+	    fromMe: isPublic,
+	    desc: "downloading your YouTube videos",
+            type: "downloader",
+            },
+		async (message, match) => {
+                if(!match) return await message.reply("_enter song url/name_")			
+		return await getVideo(message, match);
 
 })
 
