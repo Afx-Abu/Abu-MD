0090e11d60d4cd2c07de84ce1191da8664e7db46
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-04-13 06:31:18 +0530

    fix

--- plugins/song.js
+++ plugins/song.js
@@ -1,1 +1,20 @@
+const {
+  Module,
+  isPublic,
+  getSong
+} = require("../lib/");
+
+Module
+	(
+		{
+            pattern: "song?(.*)",
+	    fromMe: isPublic,
+	    desc: "show your menu items",
+            type: "downloader",
+            },
+		async (message, match) => {
+                if(!match) return await message.reply("_enter song url/name_")			
+		return await getSong(message);
+
+})
 
