1467d2aec52db94636b2aec90011af0d3eb6decb
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-04-16 15:37:12 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -10,7 +10,7 @@ const {
 Module
 	(
 		{
-            pattern: "play?(.*)",
+            pattern: "song?(.*)",
 	    fromMe: isPublic,
 	    desc: "downloading your YouTube song",
             type: "downloader",
@@ -18,34 +18,6 @@ Module
 		async (message, match) => {
                 if(!match) return await message.reply("_enter song url/name_")			
 		return await getSong(message, match);
-
-})
-
-Module
-	(
-		{
-            pattern: "yts?(.*)",
-	    fromMe: isPublic,
-	    desc: "downloading your YouTube song",
-            type: "downloader",
-            },
-		async (message, match) => {
-                if(!match) return await message.reply("_enter youthube url/name_")			
-		return await getYts(message, match);
-
-})
-
-Module
-	(
-		{
-            pattern: "video?(.*)",
-	    fromMe: isPublic,
-	    desc: "downloading your YouTube videos",
-            type: "downloader",
-            },
-		async (message, match) => {
-                if(!match) return await message.reply("_enter song url/name_")			
-		return await getVideo(message, match);
 
 })
 
