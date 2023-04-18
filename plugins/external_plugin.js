acfe5a6a8e5847b010b047820e4da0ff4fc073de
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-04-18 20:27:44 +0530

    Update external_plugin.js

--- plugins/external_plugin.js
+++ plugins/external_plugin.js
@@ -1,116 +1,107 @@
 const {
-    Module
-} = require('../lib/');
-const Config = require('../config');
-const axios = require('axios');
-const fs = require('fs');
-const Db = require('../lib/db/plugins');
-let {
-    getString
-} = require('../lib/lang');
-let Lang = getString('external_plugin');
-var handler = Config.HANDLERS !== '^'?Config.HANDLERS.split("")[0]:""
+  PluginDB,
+  setPlugin,
+  getPlugin,
+  delPlugin,
+} = require("../lib/db/plugins");
+const {
+  Module,
+} = require("../lib/");
+const fs = require("fs");
+const got = require("got");
+const axios = require("axios");
+const { format } = require("util");
+var handler = Config.HANDLERS !== 'false'?Config.HANDLERS.split("")[0]:""
 
-Module({
-    pattern: 'plugin ?(.*)',
+Module(
+  {
+    pattern: "plugin ?(.*)",
     fromMe: true,
-    type: 'owner',
-    desc: Lang.INSTALL_DESC
-}, (async (message, match) => {
-    match = match[1]!==""?match[1]:message.reply_message.text
-    if (!match || !/\bhttps?:\/\/\S+/gi.test(match)) return await message.reply(Lang.NEED_URL)
-    let links = match.match(/\bhttps?:\/\/\S+/gi);
-    for (let link of links){
-    try {
-        var url = new URL(link);
-    } catch {
-        return await message.reply(Lang.INVALID_URL);
+    desc: "Installs External plugins",
+    type: "owner",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text
+    if (!match && match !== "list") return await message.reply("_Example :_\nplugin url\nplugin list")
+    if (match == "list") {
+    const plugins = await getPlugin();
+    if (!plugins) return await message.reply("_Plugins not installed_");
+    let msg = "";
+    plugins.map(({ name, url }) => { msg += `*${name}* : ${url}\n` })
+    return await message.reply(msg);
     }
-    if (url.host === 'gist.github.com' || url.host === 'gist.githubusercontent.com') {
-        url = !url?.toString().endsWith('raw')?url.toString() + '/raw':url.toString()
+    let links = match.match(/\bhttps?:\/\/\S+/gi);
+    if (!links) {
+    const getplugin = await getPlugin(match);
+    if (!getplugin) return await message.reply("_Plugins not installed_");
+    let jsl = "";
+    getplugin.map(({ url }) => { jsl += url })
+    return await message.reply(jsl);
+    }
+    for (let link of links) {
+    try {
+      var url = new URL(link);
+    } catch {
+      return await message.reply("_Invalid url!_");
+    }
+    if (url.host === "gist.github.com") {
+        url.host = "gist.githubusercontent.com";
+        url = url.toString() + "/raw"
     } else {
         url = url.toString()
     }
     try {
-        var response = await axios(url+"?timestamp="+new Date());
+      var response = await axios(url + "?timestamp=" + new Date());
     } catch {
-        return await message.reply(Lang.INVALID_URL)
+      return await message.reply("_Invalid url!_")
     }
     let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.data)
-    var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g)?response.data.match(/pattern: ["'](.*)["'],/g).map(e=>e.replace("pattern","").replace(/[^a-zA-Z]/g, "")):"temp"
-    try { plugin_name = plugin_name[1].split(" ")[0] } catch { return await message.replyReply("_Invalid plugin. No plugin name found!_") }
-    fs.writeFileSync('./plugins/' + plugin_name + '.js', response.data);
-    plugin_name_temp = plugin_name_temp.length > 1 ? plugin_name_temp.join(", ") : plugin_name;
+    var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g) ? response.data.match(/pattern: ["'](.*)["'],/g).map(e => e.replace("pattern", "").replace(/[^a-zA-Z]/g, "")) : "temp"
     try {
-        require('./' + plugin_name);
+      plugin_name = plugin_name[1].split(" ")[0]
+    } catch {
+      return await message.reply("_Invalid plugin. No plugin name found!_")
+    }
+    fs.writeFileSync("./plugins/" + plugin_name + ".js", response.data);
+    try {
+      require("./" + plugin_name);
     } catch (e) {
-        fs.unlinkSync(__dirname+'/'+plugin_name + '.js')
-        return await message.replyReply(Lang.INVALID_PLUGIN + e);
+      fs.unlinkSync("/root/Abu/plugins/" + plugin_name + ".js")
+      return await message.reply("_Error in plugin!_\n" + format(e));
     }
-    await Db.installPlugin(url, plugin_name);
-    await message.reply(Lang.INSTALLED.format(plugin_name_temp));
-}
-}));
+    await setPlugin(url, plugin_name);
+    await message.reply("_Newly installed plugin: " + plugin_name_temp.join(", ") + "_");
+    }
+  }
+);
 
-Module({
-    pattern: 'plugin list ?(.*)',
+Module(
+  {
+    pattern: "remove ?(.*)",
     fromMe: true,
-    type: 'owner',
-    desc: Lang.PLUGIN_DESC
-}, (async (message, match) => {
-    var plugins = await Db.PluginDB.findAll();
-    if (match[1] !== '') {
-        var plugin = plugins.filter(_plugin => _plugin.dataValues.name === match[1])
-        try {
-            await message.replyReply(`_${plugin[0].dataValues.name}:_ ${plugin[0].dataValues.url}`);
-        } catch {
-            return await message.replyReply(Lang.PLUGIN_NOT_FOUND)
-        }
-        return;
+    desc: "Remove external plugins",
+    type: "owner",
+  },
+  async (message, match) => {
+    if (!match) return await message.reply("_Example :_\nremove emoji\n_remove all_")
+    if (match == "all") { 
+      await delPlugin()
+      return await message.send("_All plugins deleted Successfully_");
     }
-    var msg = Lang.INSTALLED_PLUGINS;
-    var plugins = await Db.PluginDB.findAll();
-    if (plugins.length < 1) {
-        return await message.reply(Lang.NO_PLUGIN);
-    } else {
-        plugins.map(
-            (plugin) => {
-                msg += '*' +plugin.dataValues.name + '* : ' +( plugin.dataValues.url.endsWith("/raw")?plugin.dataValues.url.replace('raw',''):plugin.dataValues.url) + '\n\n';
-            }
-        );
-        return await message.replyReply(msg);
-    }
-}));
-
-Module({
-    pattern: 'remove(?: |$)(.*)',
-    fromMe: true,
-    type: 'owner',
-    desc: Lang.REMOVE_DESC
-}, (async (message, match) => {
-    if (match[1] === '') return await message.reply(Lang.NEED_PLUGIN);
-    var plugin = await Db.PluginDB.findAll({
-        where: {
-            name: match[1]
-        }
-    });
-    if (plugin.length < 1) {
-        return await message.reply(Lang.NO_PLUGIN);
-    } else {
-        await plugin[0].destroy();
-        const Message = Lang.DELETED.format(match[1])
-        await message.replyReply(Message);
-        delete require.cache[require.resolve('./' + match[1] + '.js')]
-        fs.unlinkSync('./plugins/' + match[1] + '.js');
-          const buttons = [{buttonId: handler+'restart', buttonText: {displayText: 'Restart'}, type: 1}]
+    const isDeleted = await delPlugin(match)
+    if (!isDeleted) return await message.reply(`_Plugin ${match} not found_`);
+    delete require.cache[require.resolve("./" + match + ".js")]
+    fs.unlinkSync("./plugins/" + match + ".js");
+    await message.reply(`*_Plugin ${match} deleted_*`);
+    const buttons = [{buttonId: handler+'restart', buttonText: {displayText: 'Restart'}, type: 1}]
           
           const buttonMessage = {
-              text: Lang.DELETED.format(match[1]),
-              footer: '_Restart to make effect_',
+              text: 'Plugin Manager',
+              footer: `_Removed Plugin: ${match}_`,
               buttons: buttons,
               headerType: 1
           }
         await message.client.sendMessage(message.jid,buttonMessage);
-    }
-}));
-      
+  }
+);
+
