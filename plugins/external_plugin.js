7cc4c5dc33342069815a63c98a6157690e886eeb
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-04-18 20:31:56 +0530

    Update external_plugin.js

--- plugins/external_plugin.js
+++ plugins/external_plugin.js
@@ -12,6 +12,7 @@ const got = require("got");
 const axios = require("axios");
 const { format } = require("util");
 var handler = Config.HANDLERS !== 'false'?Config.HANDLERS.split("")[0]:""
+const Config = require('../config');
 
 Module(
   {
