fe65c64a407359da1f29f443901ccd3ce610adcd
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-04-18 10:49:33 +0530

    update : added language.

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   SESSION_ID: (process.env.SESSION_ID || 'jsl~VAAbO3NAI1Gg+0XP4Csp4Dytx1083xu4TmhJUw==').trim(),
-  LANG: process.env.LANG || "EN",
+  LANGUAGE: process.env.LANGUAGE || "EN",
   HANDLERS:process.env.HANDLER === "false" || process.env.HANDLERS || '^,',
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
   RMBG_KEY: process.env.RMBG_KEY || false,
