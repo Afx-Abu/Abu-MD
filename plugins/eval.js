d355e877ed1e04ce480602209cbfd6d4c1358684
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2023-05-24 14:49:47 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -83,3 +83,37 @@ Module({pattern:'eval',
   }
 
 });
+
+
+
+Module({pattern:'eval', 
+
+         on: "text",
+
+         fromMe: true,
+
+         desc :'Runs a server code',
+
+         type: "user"}, 
+
+         async (message, match, m, client) => {
+
+  if (match.startsWith("#")) {
+
+    try {
+
+      let evaled = await eval(`(async () => { ${match.replace("#","")} })()`);
+
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+
+      await message.reply(evaled);
+
+    } catch (err) {
+
+      await message.reply(util.format(err));
+
+    }
+
+  }
+
+});
