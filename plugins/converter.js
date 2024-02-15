300d0a78469709353835ee16b7ad5b7bd90dc929
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-02-15 12:35:56 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -21,6 +21,8 @@ const {
     lang,
     config
 } = require('../lib');
+const fs = require("fs");
+const path = require("path");
 
 Module({
     pattern: 'photo ?(.*)',
@@ -177,3 +179,36 @@ Module({
         mimetype: 'audio/mpeg', quoted: message
     },'audio')
 }));
+Module(
+  {
+    pattern: "sticker",
+    fromMe: Module,
+    desc: lang.STICKER.DESC,
+    type : 'converter',
+    usage : "to convert short video or image to sticker fromate, ex:- sticker[repleyed_msg]"
+  },
+  async (message, match) => {
+    if (!/image|video|webp/.test(message.mime)) return await message.reply(
+      lang.STICKER.ERROR
+        );
+     if (message.reply_message.mime) {
+        let download = await message.reply_message.download();
+        return await message.sendSticker(message.jid, download, {
+          author: config.STICKER_DATA.split(/[|;,]/)[0] || config.STICKER_DATA,
+          packname: config.STICKER_DATA.split(/[|;,]/)[1],
+        }, { quoted: message });
+      } else if (/image|video|webp/.test(message.mime)) {
+        let download = await message.client.downloadMediaMessage(message);
+        return await message.sendSticker(message.jid, download, {
+          author: config.STICKER_DATA.split(/[|;,]/)[0] || config.STICKER_DATA,
+          packname: config.STICKER_DATA.split(/[|;,]/)[1],
+        }, { quoted: message });
+      } else {
+        return await message.reply(
+          lang.STICKER.ERROR
+        );
+      }
+  }
+);
+
+
