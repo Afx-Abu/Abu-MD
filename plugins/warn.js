eeb5a81e5ad8753aba0bf4a968c6d7808f1d883d
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2024-02-15 11:00:11 +0530

    Create warn.js

---
+++ plugins/warn.js
@@ -0,0 +1,98 @@
+const {
+    Module,
+    groupDB,
+    lang,
+    isAdmin,
+    isBotAdmin,
+    config
+} = require('../lib');
+
+
+Module({
+    pattern: 'warn ?(.*)',
+    desc: lang.WARN.DESC,
+    type: 'action',
+    fromMe: true,
+    onlyGroup: true
+}, async (message, match) => {
+    if (!match && !message.reply_message.sender) return await message.reply(lang.WARN.METHODE.format('warn', 'warn', 'warn'));
+    if (match == 'get') {
+        const {
+            warn
+        } = await groupDB(['warn'], {
+            jid: message.jid,
+            content: {}
+        }, 'get');
+        if (!Object.keys(warn)[0]) return await message.reply('_Not Found!_');
+        let msg = '';
+        for (const f in warn) {
+            msg += `_*user:* @${f}_\n_*count:* ${warn[f].count}_\n_*remaining:* ${config.WARNCOUND - warn[f].count}_\n\n`;
+        }
+        return await message.reply(msg, {mentions: [message.reply_message.sender]});
+    } else if (match == 'reset') {
+        if (!message.reply_message.sender) return await message.reply(lang.BASE.NEED.format('user'));
+        const {
+            warn
+        } = await groupDB(['warn'], {
+            jid: message.jid,
+            content: {}
+        }, 'get');
+        if (!Object.keys(warn)[0]) return await message.reply('_Not Found!_');
+        if (!Object.keys(warn).includes(message.reply_message.number)) return await message.reply('_User Not Found!_');
+        await groupDB(['warn'], {
+            jid: message.jid,
+            content: {
+                id: message.reply_message.number
+            }
+        }, 'delete');
+        return await message.reply(lang.BASE.SUCCESS);
+    } else {
+        const BotAdmin = await isBotAdmin(message);
+        const admin = await isAdmin(message);
+        if (!BotAdmin) return await message.reply(lang.GROUP.BOT_ADMIN);
+        if (config.ADMIN_SUDO_ACCESS != 'true' && !message.isCreator) return await message.reply(lang.BASE.NOT_AUTHR)
+        if (!admin && !message.isCreator) return await message.reply(lang.BASE.NOT_AUTHR)
+        if (!message.reply_message.sender) return await message.reply(lang.BASE.NEED.format('user'));
+        const reason = match || 'warning';
+        const {
+            warn
+        } = await groupDB(['warn'], {
+            jid: message.jid,
+            content: {}
+        }, 'get');
+        const count = Object.keys(warn).includes(message.reply_message.number) ? Number(warn[message.reply_message.number].count) + 1 : 1;
+        await groupDB(['warn'], {
+                jid: message.jid,
+                content: {
+                    [message.reply_message.number]: {
+                        count
+                    }
+                }
+            },
+            'add');
+        const remains = config.WARNCOUND - count;
+        let warnmsg = `╭─❏ ❮ *ᴡᴀʀɴɪɴɢ* ❯ ❏
+│ _*ᴜsᴇʀ : @${message.reply_message.number}⁩*_
+│ _*ᴡᴀʀɴ : ${count}*_
+│ _*ʀᴇᴀsᴏɴ : ${reason}*_
+│ _*ʀᴇᴍᴀɪɴɪɴɢ : ${remains}*_
+╰─❏`
+
+        await message.reply(warnmsg, {
+            mentions: [message.reply_message.sender]
+        })
+        if (remains <= 0) {
+            await groupDB(['warn'], {
+                jid: message.jid,
+                content: {
+                    id: message.reply_message.number
+                }
+            }, 'delete');
+            if (BotAdmin) {
+                await message.client.groupParticipantsUpdate(message.from, [message.reply_message.sender], 'remove');
+                return await message.reply(lang.WARN.MAX)
+            };
+        };
+    };
+})
+  
